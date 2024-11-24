68067b0af74fe388ebcc1e371eb2cc9a6cb9560f
maxleal2 <103587041+maxleal2@users.noreply.github.com>
2024-11-24 16:43:07 -0600

    Created graph to visualize price with Moving Averages

--- fetchData.py
+++ fetchData.py
@@ -25,4 +25,13 @@ data['200-Day MA']= data['Close'].rolling(window=200).mean()
 
 # adding 30 day volatility
 data['30-Day Volatility'] = data['Daily Return'].rolling(window=30).std() * (252**.05) # annualized vol
-print(data.tail())
+# print(data.tail())
+
+# plotting price with moving avgs
+plt.figure(figsize=(10,6))
+plt.plot(data['Close'], label='Close Price')
+plt.plot(data['50-Day MA'], label='50-Day Moving Average')
+plt.plot(data['200-Day MA'], label='200-Day Moving Average')
+plt.title('Stock Price and Moving Averages')
+plt.legend()
+plt.show()
