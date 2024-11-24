989adf036905dfa7ee05de5f53b310f5037b33b2
maxleal2 <103587041+maxleal2@users.noreply.github.com>
2024-11-24 16:34:18 -0600

    Added daily return

--- fetchData.py
+++ fetchData.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 # choose a stock symbol 
 stock = yf.Ticker('MSTR')
@@ -12,3 +13,12 @@ data = stock.history(period='5y')
 
 # fill any missing values using the forward fill method
 data = data.fillna(method='ffill')
+
+# now to calculate metrics 
+
+# calculate daily returns as percentage from (dayClose - dayBeforeClose)/dayBeforeClose
+data['Daily Return'] = data['Close'].pct_change()
+
+#calculating 50 and 200 day moving avgs 
+# data['50']
+print(data.tail())
